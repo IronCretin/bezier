ffc88b2375bf95b8d8e058c27c16d53f5d35bc92
Paul Maynard <palu.maynard@gmail.com>
2019-08-02 23:43:37 -0400

    real formulas instead of half-figured out stuff i did on my own
    I cracked and actually read the wiki article

---
+++ bezier.r
@@ -0,0 +1,25 @@
+pts <- matrix(
+  c(
+    0, 0,
+    0, 1,
+    1, 0),
+  nrow = 3,
+  ncol = 2,
+  byrow = TRUE)
+
+basis <- function(n, v, t) {
+  return(choose(n, v) * (t^v) * (1-t)^(n-v))
+}
+
+n <- nrow(pts)-1
+i <- 0:n
+
+curvex <- function(t) sum(pts[,1] * basis(n, i, t))
+curvey <- function(t) sum(pts[,2] * basis(n, i, t))
+
+path <- data.frame(t=seq(0, 1, by=0.05))
+
+path$x = sapply(path$t, curvex)
+path$y = sapply(path$t, curvey)
+
+with(path, plot(x,y, type="l"))
